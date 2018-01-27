c97fdbe904d512ea9eeedb2af0b32f692d8fd531
magkor624 <magkor624@gmail.com>
2018-01-28 00:37:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/halloandroid/HalloAndroid.java
@@ -0,0 +1,13 @@
+package com.example.android.halloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo_android);
+    }
+}
